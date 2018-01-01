7e2ea7dfb576f050f94c6efc2c2094db6388fb49
Jiawei Zhang <809681148@qq.com>
2017-12-31 20:36:08 -0500

    able to get the api data

--- Spotify.js
+++ Spotify.js
@@ -1,9 +1,9 @@
 // referene from https://developer.spotify.com/web-api/authorization-guide/
 // this is build base on the instruction , the flow of 
-var express = require('express'); // Express web server framework
-var request = require('request'); // "Request" library
+var express     = require('express'); // Express web server framework
+var request     = require('request'); // "Request" library
 var querystring = require('querystring');
-
+var utf8        = require("utf8");
 
 const CLIENT_ID     = 'c778e8173793481c907f2ee677fdf578'; // Your client id
 const CLIENT_SECRET = '3d5d8daa997a4b29b11100d55b018ad2'; // Your secret
@@ -15,5 +15,28 @@ module.exports = {
     CLIENT_ID,
     CLIENT_SECRET,
     REDIRECT_URI,
-    SCOPE
+    SCOPE,
+    get_song_info
+}
+
+function get_song_info(track,artirst,access_token){
+    var options = {
+        url: "https://api.spotify.com/v1/search?q="+track+"&type=track",
+        headers: { 'Authorization': 'Bearer ' + access_token},
+        json: true
+      };
+    
+    request.get(options,(error, response, body)=> {
+        // console.log(body)
+        // res.send(body);
+        body.tracks.items.forEach(element => {
+            // console.log("!!!",JSON.stringify(body.tracks.items[0]))
+
+            if(element['artists'][0]['name'].toLowerCase() === artirst.toLowerCase()){
+                console.log(element.id,element.uri)
+            }
+
+        //     }
+        });
+    })
 }
--- main.js
+++ main.js
@@ -1,7 +1,8 @@
+
 var express = require('express'); // Express web server framework
 var request = require('request'); // "Request" library
 var querystring = require('querystring');
-const spotify   = require("./Spotify");
+const Spotify   = require("./Spotify");
 const app = express();
 
 app.get("/login",(req,res) => {
@@ -9,21 +10,50 @@ app.get("/login",(req,res) => {
      * @param  {} nothing
      * @return {promise} 
      */
-
      var url = 'https://accounts.spotify.com/authorize?';
 
     var QUERY_PARAMETER = {
-        client_id : spotify.CLIENT_ID,
+        client_id : Spotify.CLIENT_ID,
         response_type : "code",
-        redirect_uri : spotify.REDIRECT_URI,
-        scope : spotify.SCOPE
+        redirect_uri : Spotify.REDIRECT_URI,
+        scope : Spotify.SCOPE
       }
-
     res.redirect(url + querystring.stringify(QUERY_PARAMETER))
 })
 
 
 app.get("/callback",(req,res) => {
-    res.send("well come back");
+    var code = req.query.code  || null;
+    var state= req.query.state || null;
+
+    if(code == null)
+        res.send("something goes wrong during stage 2-3");
+    
+    // requests_refresh_and_access_tokens
+    REQUEST_BODY_PARAMETER = {
+        url :'https://accounts.spotify.com/api/token',
+        form : {
+            grant_type  :"authorization_code",
+            code        : code,
+            redirect_uri: Spotify.REDIRECT_URI
+        },
+        headers: {
+            'Authorization': 'Basic ' + (new Buffer(Spotify.CLIENT_ID + ':' + Spotify.CLIENT_SECRET).toString('base64'))
+        },
+        json:true
+    }
+
+    request.post(REQUEST_BODY_PARAMETER, (error, response, body) =>{
+        // in our case, I do not think by any chane we will use refresh_token simply casue there is not that many data to process
+        var access_token  = body.access_token,
+            refresh_token = body.refresh_token;
+        // res.send(access_token);
+        Spotify.get_song_info("friends",'Justin Bieber',access_token);
+        res.send(access_token)
+        // add_song_into_spotify(access_token,refresh_token)
+
+    })
+    
 })
+
 app.listen(8888);
