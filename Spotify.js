07d642cec4d96d3d683b9eb6832b3f635127d5df
Jiawei Zhang <809681148@qq.com>
2018-01-17 15:08:44 -0500

    made some good process on convert to request promise

--- Spotify.js
+++ Spotify.js
@@ -1,3 +1,6 @@
+import { Promise } from '../../../Library/Caches/typescript/2.6/node_modules/@types/bluebird';
+
+
 // referene from https://developer.spotify.com/web-api/authorization-guide/
 // this is build base on the instruction , the flow of 
 var express     = require('express'); // Express web server framework
@@ -9,6 +12,7 @@ const CLIENT_SECRET = '3d5d8daa997a4b29b11100d55b018ad2'; // Your secret
 const REDIRECT_URI  = 'http://localhost:8888/callback'; // Your redirect uri
 const SCOPE         = 'playlist-modify-public playlist-read-collaborative'
 const playlist_name = "tmp"
+const rp            = require("request-promise");
 
 // in spotify , 戴佩妮 been record as Penny Tai, her song is ony avaliable if I search with offical name, use a dic to stoore this information 
 var artist_dic = {}
@@ -26,81 +30,81 @@ module.exports = {
     get_playlist_id,
     add
 }
-function get_artist_offical_name(artist,access_token,callback){
 
-    if(artist_dic[artist]){
-        callback(artist_dic[artist])
-    }else{
-        var options = {
-            url: "https://api.spotify.com/v1/search?q="+encodeURIComponent(artist)+"&type=artist",
-            headers: { 'Authorization': 'Bearer ' + access_token},
-            json: true
-          };
-    
-        request.get(options,(error,response,body) => {
-            if(body.artists && body.artists.items.length != 0){
-                artist_dic[artist] = body.artists.items[0].name;
-                artist             = body.artists.items[0].name;
-            }
-                callback(artist);    
-        });
-    }
-}
-
-function get_song_id(track,artist,access_token,callback){
-    /**
-     * get_song_id : get the track id base on track astrist 
-     * para @{String, String , String ,function}
-     * https://developer.spotify.com/web-api/search-item/
+function get_artist_offical_name(artist,access_token) {
+    /***
+     * during spotify searching, it is more accurate to get the official name that spotify recorded
+     * the search return a new promise with artist offical name 
      */
-    get_artist_offical_name(artist,access_token,(name) => {
-        console.log("official name" , name);
-        var options = {
-            url: "https://api.spotify.com/v1/search?q="+encodeURIComponent(track)+"&type=track",
-            headers: { 'Authorization': 'Bearer ' + access_token},
-            json: true
-          };
-        
-        request.get(options,(error, response, body)=> {
-            // console.log(body.tracks)
-            if(response.statusCode !== 400 && body.tracks){
-                for(var i = 0; i< body.tracks.items.length; i++){
-                    element = body.tracks.items[i];
-                    if(encodeURIComponent(element['artists'][0]['name'].toLowerCase()) === encodeURIComponent(name.toLowerCase())){
-                            callback(element.id,element.uri)    
-                        break;
-                    }
-                }
-            }
+    var options = {
+        url: "https://api.spotify.com/v1/search?q="+encodeURIComponent(artist)+"&type=artist",
+        headers: { 'Authorization': 'Bearer ' + access_token},
+        json: true
+    };
+
+    return new Promise((resolve,reject) => {
+        rp(options)
+        .then((body) => {
+            if(body.artists && body.artists.items.length != 0)
+                resolve(body.artists.items[0].name);
+            else
+                reject("Unable to find artirst" + artist);
         })
     })
-
+    .catch((err) =>{console.log("error during get_artist_offical_name" + err)})
 }
 
-function get_user_id(access_token,callback){
-    /**
-     * get_user_id : get username of current user
-     * @(String,function)
-     * https://developer.spotify.com/web-api/get-current-users-profile/
-     */
+
+function get_song_id(track,artist,access_token){
+    return new Promise((resolve,reject) => {
+        get_artist_offical_name(artist,access_token)
+        .then((name) => {
+            console.log(name,"1")
+            var options = {
+                url: "https://api.spotify.com/v1/search?q="+encodeURIComponent(track)+"&type=track",
+                headers: { 'Authorization': 'Bearer ' + access_token},
+                json: true
+            };
+
+            rp(options)
+            .then((body) => {
+                // console.log(body)
+                if(body.tracks && body.tracks.items) {
+                    for(var i = 0; i< body.tracks.items.length; i++) {
+                        element = body.tracks.items[i];
+                        // console.log(name)
+                        if(encodeURIComponent(element['artists'][0]['name'].toLowerCase()) === encodeURIComponent(name.toLowerCase())) {
+                            resolve(element.id);
+                            break;
+                        }
+                    }
+                    reject(`Unable to find data for ${track} ${name}`)
+                }
+                reject(`Unable to find data for ${track} ${name}`)
+            })
+        })
+    })
+    .catch((err) => {console.log("error during get_song_id : " + err)})
+}
+
+
+function get_user_id(access_token) {
     var options = {
         url: "https://api.spotify.com/v1/me",
         headers: { 'Authorization': 'Bearer ' + access_token},
         json: true
-      };   
-    
-      request.get(options,(error,response,body) => {
-        callback(body.id);
-      });
+      };
+      return new Promise((resolve,reject) => {
+        rp(options)
+        .then((body) => {
+            if(body.id)
+                resolve(body.id)
+            reject("Unable to get user id")
+        })
+      })
 }
 
-function create_playlist(user_id,access_token,callback){
-    /**
-     * create_playlist : create a playlist if playlistname not exist 
-     * @(String,function)
-     * https://developer.spotify.com/web-api/create-playlist/
-     */
-    
+function create_playlist(user_id,access_token){
     var options = {
         url : "https://api.spotify.com/v1/users/"+user_id+"/playlists",
         headers: {
@@ -108,36 +112,63 @@ function create_playlist(user_id,access_token,callback){
         },
         json : true
     }
-
-    request.get(options,(error,response,body) =>{
-        if(response.statusCode !== 400){
-            var playlists = body.items.map(x => x.name)
-            // check if playlist already exist 
-            if(!playlists.includes(playlist_name)){
-                var options = {
-                    url: 'https://api.spotify.com/v1/users/' + user_id + '/playlists',
-                    body: JSON.stringify({
-                        'name': playlist_name,
-                        'public': true
-                    }),
-                    dataType:'json',
-                    headers: {
-                        'Authorization': 'Bearer ' + access_token,
-                        'Content-Type': 'application/json',
-                    }
-                };
-                // create playlist if not 
-                request.post(options,(error,response,body) => {
-                    console.log("playlist created");
-                    callback();
-                })
-            }else{
-                callback() 
+    return new Promise((resolve,reject) => {
+        rp(options)
+        .then((body) => {
+            if(body && body.items){
+                var playlists = body.items.map(x => x.name)
             }
-        }
-
+            
         })
-    }
+    })
+
+    
+
+}
+// function create_playlist(user_id,access_token,callback){
+//     /**
+//      * create_playlist : create a playlist if playlistname not exist 
+//      * @(String,function)
+//      * https://developer.spotify.com/web-api/create-playlist/
+//      */
+    
+//     var options = {
+//         url : "https://api.spotify.com/v1/users/"+user_id+"/playlists",
+//         headers: {
+//             'Authorization': 'Bearer ' + access_token,
+//         },
+//         json : true
+//     }
+
+//     request.get(options,(error,response,body) =>{
+//         if(response.statusCode !== 400){
+//             var playlists = body.items.map(x => x.name)
+//             // check if playlist already exist 
+//             if(!playlists.includes(playlist_name)){
+//                 var options = {
+//                     url: 'https://api.spotify.com/v1/users/' + user_id + '/playlists',
+//                     body: JSON.stringify({
+//                         'name': playlist_name,
+//                         'public': true
+//                     }),
+//                     dataType:'json',
+//                     headers: {
+//                         'Authorization': 'Bearer ' + access_token,
+//                         'Content-Type': 'application/json',
+//                     }
+//                 };
+//                 // create playlist if not 
+//                 request.post(options,(error,response,body) => {
+//                     console.log("playlist created");
+//                     callback();
+//                 })
+//             }else{
+//                 callback() 
+//             }
+//         }
+
+//         })
+//     }
 
 
 function get_playlist_id(access_token,callback){
@@ -213,3 +244,12 @@ function add(track,artist,user_id,access_token){
             })
         })            
 }
+
+
+// get_artist_offical_name("Justin bieber" , "BQClCYXKZ_AUHkiJ-LrR5AH0Qv872VEzz7C0YIxDwL7xwyeJgtoykeVGaQNoDdqTKCZgu5suapI5FydfTgx69UYoQTzh9wEQOSb_um7Fj949hbrkXHl-UeweRfAWRX9FQzxqqfcWwMPlw3dAFBcXAN2vHjwAyE19NbwyCc4pN41W2kN6fw")
+
+// get_song_id("Love yourself","Justin bieber" ,"BQClCYXKZ_AUHkiJ-LrR5AH0Qv872VEzz7C0YIxDwL7xwyeJgtoykeVGaQNoDdqTKCZgu5suapI5FydfTgx69UYoQTzh9wEQOSb_um7Fj949hbrkXHl-UeweRfAWRX9FQzxqqfcWwMPlw3dAFBcXAN2vHjwAyE19NbwyCc4pN41W2kN6fw")
+// .then((res)=>{console.log(res)})
+// .catch((error) => {console.log(error)})
+get_user_id("BQClCYXKZ_AUHkiJ-LrR5AH0Qv872VEzz7C0YIxDwL7xwyeJgtoykeVGaQNoDdqTKCZgu5suapI5FydfTgx69UYoQTzh9wEQOSb_um7Fj949hbrkXHl-UeweRfAWRX9FQzxqqfcWwMPlw3dAFBcXAN2vHjwAyE19NbwyCc4pN41W2kN6fw")
+.then((res)=>{console.log(res)})
--- test.js
+++ test.js
@@ -1,9 +1,17 @@
-function foo(x,callback) {
-    for(i = 0; i < 10; i ++)
-        callback(i);
+function foo(ls) {
+    return new Promise((resolve,reject) => {
+        if(ls === 10)
+            resolve(10)
+        reject(1)
+    })
 }
 
-foo(10,(i) => {
-    console.log(i);
-})
+foo(10)
+.then((num) => {
+    return new Promise((resolve,reject) => {
 
+    }
+})
+.catch((err) => {
+    console.log("error");
+})
