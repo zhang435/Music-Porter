dbe19b42b89caf5578def3eded76f12936ebfab6
Jiawei Zhang <809681148@qq.com>
2018-02-18 12:40:23 -0500

    change redirect url from localhost to www

--- Spotify.js
+++ Spotify.js
@@ -6,7 +6,8 @@ var querystring = require('querystring');
 
 const CLIENT_ID = 'c778e8173793481c907f2ee677fdf578'; // Your client id
 const CLIENT_SECRET = '3d5d8daa997a4b29b11100d55b018ad2'; // Your secret
-const REDIRECT_URI = 'http://localhost:8888/callback'; // Your redirect uri
+const url = "https://still-brushlands-47642.herokuapp.com/"
+const REDIRECT_URI = url + "callback"; // Your redirect uri
 const SCOPE = 'playlist-modify-public playlist-read-collaborative playlist-modify-private'
 const playlist_name = "tmp"
 const rp = require("request-promise");
