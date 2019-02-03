721d784f5ce6445a323cdbfe3d247192effc5600
Jiawei Zhang <809681148@qq.com>
2019-02-03 15:06:39 -0500

    add url reg validation

--- src/js/validation.js
+++ src/js/validation.js
@@ -1,8 +1,28 @@
-function validation() {
+function xiamiValidation() {
+    /**
+     * validator for url from user
+     */
+
     var url = document.getElementById("xiamiUrl").value;
-    if (!url.includes("page/2?spm")) {
+    var reg = /https:\/\/www.xiami.com\/space\/lib-song\/u\/\d+\/page\/\d\?spm=\S+/g;
+    if (url.match(reg) == null) {
         alert("Invalid url, it has to be the url of second page of your playlist");
         return false;
     }
     return true;
 }
+
+function neteaseValidation() {
+    var url = document.getElementById("neteaseUrl").value;
+    var reg = /https:\/\/music.163.com\/playlist\?id=\d+/g;
+
+
+    if (url.match(reg) == null) {
+        alert("Invalid url, Please check the url");
+        return false;
+    };
+    return true;
+}
+var url = "https://www.xiami.com/s/18313828/page/2?spm=a1z1s.6928797.1561534521.342.HmvvYd"
+var reg = /https:\/\/www.xiami.com\/space\/lib-song\/u\/\d+\/page\/\d\?spm=\S+/g;
+console.log(url.match(reg) == null)
